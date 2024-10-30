964a1436d0598a9d3f7f46f16ffbb7bc4c1be616
Tarun <tarundiwaker977@gmail.com>
2024-10-29 22:45:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+// Initialize Swiper 
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  // Menu open close 
+  let menu = document.querySelector('.menu-icon');
+  let navbar = document.querySelector('.navbar');
+
+  menu.onclick = () => {
+     menu.classList.toggle("move");
+     navbar.classList.toggle('open-menu')
+  };
+
+//  close menu on scroll
+window.onscroll = () => {
+  menu.classList.remove("move");
+  navbar.classList.remove('open-menu')
+};
+// Scrollreveal  Animation
+const animate = ScrollReveal({
+origin: 'top',
+distance: '60px',
+duration: '2500',
+delay: '400',
+});
+
+animate.reveal(".nav");
+animate.reveal(".home-text", {origin: "left"});
+animate.reveal(".home-img", {origin: "bottom"});
+animate.reveal(".ser-box,.product-box,.team-box,.book-data", {interval: 100});
+
